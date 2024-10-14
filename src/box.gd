a754e120d6fe7a7e840ec0daa6a9227a6657147b
tachibana-marine <d.ageha.sekaijyu1041@gmail.com>
2024-10-15 00:43:20 +0900

    Add basic movement to Box

--- src/box.gd
+++ src/box.gd
@@ -2,14 +2,19 @@ extends CharacterBody3D
 class_name Box
 
 const SPEED = 5.0
+const DASH_SPEED = 10.0
 const JUMP_VELOCITY = 4.5
+const FLICTION = 0.1
 
-var jump_height: float = 100:
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity"):
+	get:
+		return gravity
+		
+var jump_height: float = 2:
 	get:
 		return jump_height
 	set(value):
 		jump_height = value
-
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -18,16 +23,25 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
+		velocity.y = sqrt(abs(2 * gravity * jump_height))
+	elif (Input.is_action_just_released("jump") and not is_on_floor()):
+		velocity.y = 0
+	
+		
 	# Get the input direction and handle the movement/deceleration.
+	var speed = SPEED
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var dash_dir = Input.get_vector("dash_left", "dash_right", "dash_forward", "dash_backward")
+	if (not dash_dir.is_zero_approx()):
+		input_dir = dash_dir
+		speed = DASH_SPEED
+	
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = lerp(velocity.x, 0.0, FLICTION)
+		velocity.z = lerp(velocity.z, 0.0, FLICTION)
 
 	move_and_slide()
--- test/unit/test_box.gd
+++ test/unit/test_box.gd
@@ -2,42 +2,106 @@ extends GutTest
 
 const FPS = 60
 const DELTA = 1.0 / FPS
+const TIME_TO_REACH_APEX_DEFAULT = 0.20
+const TIME_TO_REACH_APEX_3_METERS = 0.24
+const TIME_TO_REACH_HALF_THE_APEX_DEFAULT = 0.059
 
 var sender = InputSender.new(Input)
 var box_on_surface = load("res://test/scene/box_on_surface.tscn")
-var scene
+var scene = null
+var box = null
 
 func before_each():
 	scene = add_child_autofree(box_on_surface.instantiate())
-	simulate(scene, 1, DELTA)
+	box = scene.get_node("Box")
+	await wait_until(func(): return box.is_on_floor(), 1)
+	
 
 func after_each():
+	# Calculate and print values for parameters
+	# var v = sqrt(abs(2 * box.gravity * box.jump_height))
+	# gut.p(v / box.gravity)
+	# gut.p((v - sqrt(v * v - box.gravity * box.jump_height)) / box.gravity)
 	sender.release_all()
 	sender.clear()
 
+# expect 5% error in jump height
 func test_jump_when_jump_action_is_pressed():
-	var box = scene.get_node("Box")
 	var y = box.position.y
 	assert_true(box.is_on_floor())
-	sender.action_down("jump").hold_for(.1)
+	# sqrt(abs(2 * box.gravity * box.jump_height)) / box.gravity = 0.64
+	sender.action_down("jump").hold_for(TIME_TO_REACH_APEX_DEFAULT)
 	await (sender.idle)
 	await wait_until(func(): return not box.is_on_floor(), 1)
-	simulate(box, 3, DELTA)
-	assert_lt(y, box.position.y)
+	# wait until the box reaches the apex of the jump
+	await wait_until(func(): return box.velocity.y < 0.0, 1)
+	# default jump height is 2
+	assert_almost_eq(box.position.y, 2.0, 0.1)
 	assert_false(box.is_on_floor())
 
-func test_limit_jump_height():
-	var box = scene.get_node("Box")
-	box.jump_height = 1
+func test_setting_jump_height():
+	box.jump_height = 3
 	assert_true(box.is_on_floor())
-	sender.action_down("jump").hold_for(.1)
+	sender.action_down("jump").hold_for(TIME_TO_REACH_APEX_3_METERS)
 	await (sender.idle)
 	# wait until the box reaches the apex of the jump
-	await wait_until(func(): return box.velocity.y < 0, 1)
-	assert_lt(box.position.y, 1)
+	await wait_until(func(): return box.velocity.y < 0.0, 1)
+	assert_almost_eq(box.position.y, 3.0, 0.15)
 	
+func test_change_jump_height_depending_on_button_hold():
+	assert_true(box.is_on_floor())
+	# I did the math
+	sender.action_down("jump").hold_for(TIME_TO_REACH_HALF_THE_APEX_DEFAULT)
+	await (sender.idle)
+	await wait_until(func(): return box.velocity.y < 0.0, 1)
+	# hold for half the time to jump half the height
+	assert_almost_eq(box.position.y, 1.0, 0.05)
 
-#	var box: Box = test_scene.find_child("Box")
-#	var y_before = box.position.y
-#	test_scene.simulate_key_press(KEY_SPACE)
-#	await test_scene.await_input_processed()
+func test_move():
+	assert_true(box.is_on_floor())
+	sender.action_down("move_forward").hold_for(.1)
+	await (sender.idle)
+	assert_lt(box.velocity.z, 0.0)
+
+	box.position = Vector3.ZERO
+	sender.action_down("move_backward").hold_for(.1)
+	await (sender.idle)
+	assert_gt(box.velocity.z, 0.0)
+
+	box.position = Vector3.ZERO
+	sender.action_down("move_left").hold_for(.1)
+	await (sender.idle)
+	assert_lt(box.velocity.x, 0.0)
+
+	box.position = Vector3.ZERO
+	sender.action_down("move_right").hold_for(.1)
+	await (sender.idle)
+	assert_gt(box.velocity.x, 0.0)
+
+func test_dash():
+	# I don't care how fast the dash is.
+	assert_true(box.is_on_floor())
+	sender.action_down("dash_forward").hold_for(.1)
+	await (sender.idle)
+	assert_lt(box.velocity.z, 0.0)
+
+	box.position = Vector3.ZERO
+	sender.action_down("dash_backward").hold_for(.1)
+	await (sender.idle)
+	assert_gt(box.velocity.z, 0.0)
+
+	box.position = Vector3.ZERO
+	sender.action_down("dash_left").hold_for(.1)
+	await (sender.idle)
+	assert_lt(box.velocity.x, 0.0)
+
+	box.position = Vector3.ZERO
+	sender.action_down("dash_right").hold_for(.1)
+	await (sender.idle)
+	assert_gt(box.velocity.x, 0.0)
+
+func test_move_with_intertia():
+	assert_true(box.is_on_floor())
+	sender.action_down("move_forward").hold_for(.1)
+	await (sender.idle)
+	assert_lt(box.velocity.z, 0.0)
