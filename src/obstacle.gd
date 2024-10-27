8e16bfe020012fddc29c34b1dad68ae3b367ff5c
tachibana-marine <d.ageha.sekaijyu1041@gmail.com>
2024-10-27 23:18:26 +0900

    make obstacle a child of CollisionBox3D

--- src/obstacle.gd
+++ src/obstacle.gd
@@ -1,39 +1,10 @@
-@tool
-extends AnimatableBody3D
+extends CollisionBox3D
 class_name Obstacle
-
-@export var size: Vector3 = Vector3(1, 1, 1):
-	get:
-		return size
-	set(value):
-		size = value
-		$CollisionShape.shape.size = value
-		$Box.size = value
-
 @export var speed: int = 20:
 	get:
 		return speed
 	set(value):
 		speed = value
-
-@export var color: Color = Color(1, 1, 1):
-	get:
-		return color
-	set(value):
-		color = value
-		$Box.material.albedo_color = value
-
-# Called when the node enters the scene tree for the first time.
-func _init():
-	var box = CSGBox3D.new()
-	box.name = "Box"
-	box.material = StandardMaterial3D.new()
-	add_child(box)
-
-	var collision = CollisionShape3D.new()
-	collision.name = "CollisionShape"
-	collision.shape = BoxShape3D.new()
-	add_child(collision)
 
 
 func _physics_process(delta: float) -> void:
--- test/unit/test_obstacle.gd
+++ test/unit/test_obstacle.gd
@@ -1,34 +1,13 @@
 extends GutTest
 var obstacle_script = load("res://src/obstacle.gd")
 var obstacle = null
-var collision = null
-var collision_shape = null
-var box = null
-var box_material = null
 
 func before_each():
     obstacle = add_child_autofree(obstacle_script.new())
     add_child_autofree(CreateCamera.new().create())
-
-    collision = obstacle.get_node("CollisionShape")
-    collision_shape = collision.shape
-    box = obstacle.get_node("Box")
-    box_material = box.material
 
 func test_move_with_specified_speed():
     obstacle.speed = 40
     assert_eq(obstacle.speed, 40)
     await wait_seconds(.5)
     assert_almost_eq(obstacle.position, Vector3(0, 0, 20), Vector3(0, 0, 0.01))
-
-func test_has_size_property():
-    assert_property(obstacle, "size", Vector3(1, 1, 1), Vector3(2, 3, 4))
-
-func test_changing_size_also_changes_shape():
-    obstacle.size = Vector3(4, 2, 3)
-    assert_eq(collision.shape.size, Vector3(4, 2, 3))
-    assert_eq(box.size, Vector3(4, 2, 3))
-
-func test_obstacle_has_color_property():
-    assert_property(obstacle, "color", Color(1, 1, 1), Color(.5, .5, 0))
-    assert_eq(box_material.albedo_color, Color(.5, .5, 0))
